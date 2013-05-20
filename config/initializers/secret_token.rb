3ee8a80a0f65f68a925969f3766d8e32d4cf1297
Ray Farias <rfarias@foulweather.net>
2013-05-19 21:43:32 -1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoulSac2::Application.config.secret_token = 'c2a6f506ed760553e368d8d0fa60fc95bd7ae0239981938ec7bf8ab22b9f3a6ba7c4183c04665d27823db6da7d8d38c38590c3cf4454b64329d40f754724d2c7'
