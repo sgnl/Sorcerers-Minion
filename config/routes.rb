1c31da63fbf1b8bb79e301753305d765f9ed4876
Ray Farias <rfarias@foulweather.net>
2013-05-23 12:23:37 -1000

    add sign out function and cookie

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 SoulSac2::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   resources :missions do
     collection { post :import } 
   end
 
   root to: 'missions#index'
-  match '/signup' => 'users#new'
 
   # get "portal_pages/home"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130523083808) do
+ActiveRecord::Schema.define(:version => 20130523220707) do
 
   create_table "missions", :force => true do |t|
     t.string   "name"
@@ -32,6 +32,9 @@ ActiveRecord::Schema.define(:version => 20130523083808) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
